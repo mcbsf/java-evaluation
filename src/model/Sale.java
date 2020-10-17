b655ffd6f43a727ec24ce136ad70e4df4f964099
mcbsf@cin.ufpe.br <mcbsf@cin.ufpe.br>
2020-10-17 15:31:15 -0300

    changing sale_info type to ArrayList of ArrayLists for faster dev

--- src/model/Sale.java
+++ src/model/Sale.java
@@ -1,11 +1,13 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Sale {
 	public String id;
-	public String[] item_info;
+	public ArrayList<ArrayList<String>> item_info;
 	public String salesman;
 	
-	public Sale(String id, String[] item_info, String salesman) {
+	public Sale(String id, ArrayList<ArrayList<String>> item_info, String salesman) {
 		this.id = id;
 		this.item_info = item_info;
 		this.salesman = salesman;
