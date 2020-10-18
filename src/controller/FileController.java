0942ea5097af03f7ddd78694dbf991c73242414b
mcbsf@cin.ufpe.br <mcbsf@cin.ufpe.br>
2020-10-17 23:08:43 -0300

    add try/catch in each line processed to finish files with noisy lines

--- src/controller/FileController.java
+++ src/controller/FileController.java
@@ -72,7 +72,46 @@ public class FileController {
 		return unprocessed_files;
 	}
 
-	public static void process_file(File file) throws IOException{
+	
+
+	public static ArrayList<File> process_files(ArrayList<File> unprocessed_files) throws IOException{
+		
+		for (File file : unprocessed_files){
+		   try{
+			   process_file(file);
+		   }catch (Exception e) {
+			   LogController.log_exception(e);
+		   }
+	    }
+		return null;
+	}
+	
+	
+	
+	private static String get_worst_salesman(ArrayList<Sale> sales, ArrayList<Salesman> salesmen) {
+		// sum_sales/salary
+		Map<String, Float> salesmen_amount = get_salesmen_by_sales_amount(sales);
+		Map<String, Float> salesmen_performance = get_performance_by_salesman(salesmen_amount, salesmen);
+		
+		String worst_salesman_name = "";
+		float worst_salesman_performance = 1000000000;
+		
+		for (Map.Entry<String, Float> entry : salesmen_performance.entrySet()) {
+			
+			String actual_salesman_name = entry.getKey();
+			float actual_performance = entry.getValue();
+		    
+			if(actual_performance<worst_salesman_performance) {
+		    	worst_salesman_performance = actual_performance;
+		    	worst_salesman_name = actual_salesman_name;
+		    }
+			
+			System.out.println(entry.getKey() + "/" + entry.getValue());
+		}
+		return worst_salesman_name;
+	}
+	
+	private static void process_file(File file) throws IOException{
 		//add line_index to log mapped errors
 		System.out.println("\nprocessing file " + file.getName());
 		String[] lines = Files.readAllLines(file.toPath()).toArray(new String[0]);
@@ -87,33 +126,37 @@ public class FileController {
 		
 		//iterate over lines to build models and fill local repositories 
 		for(String line: lines) {
+			try {
+
+				System.out.println(line);
+				String[] splitted_line = line.split("ç");
+				String id = splitted_line[0];
+				
+				if(Objects.equals(id, "001")) {
+					Salesman salesman = new Salesman(splitted_line[1], splitted_line[2], splitted_line[3]);
+					salesmen.add(salesman);
+					
+				}else if(Objects.equals(id, "002")){
+					Customer customer = new Customer(splitted_line[1], splitted_line[2], splitted_line[3]);
+					customers.add(customer);
+					
+				}else if(Objects.equals(id, "003")){
+					
+					ArrayList<ArrayList<String>> sale_info = get_sale_info(splitted_line[2]);
+					
+					Sale sale = new Sale(splitted_line[1], sale_info, splitted_line[3]);
+					sale.simplify_amount();
+					System.out.println("    SALE "+ String.valueOf(sale.amount) + "\n");
+					sales.add(sale);
+					
+				}else {
+					others.add(splitted_line);
+				}
 			
-			System.out.println(line);
-			String[] splitted_line = line.split("ç");
-			String id = splitted_line[0];
 			
-			if(Objects.equals(id, "001")) {
-				Salesman salesman = new Salesman(splitted_line[1], splitted_line[2], splitted_line[3]);
-				salesmen.add(salesman);
-				
-			}else if(Objects.equals(id, "002")){
-				Customer customer = new Customer(splitted_line[1], splitted_line[2], splitted_line[3]);
-				customers.add(customer);
-				
-			}else if(Objects.equals(id, "003")){
-				
-				ArrayList<ArrayList<String>> sale_info = get_sale_info(splitted_line[2]);
-				
-				Sale sale = new Sale(splitted_line[1], sale_info, splitted_line[3]);
-				sale.simplify_amount();
-				System.out.println("    SALE "+ String.valueOf(sale.amount) + "\n");
-				sales.add(sale);
-				
-			}else {
-				others.add(splitted_line);
-			}
-		
-			
+			} catch(Exception e){
+				LogController.log_line_exception(e);
+			}	
 		}
 		ArrayList<Customer> arr_customers = new ArrayList(customers);
 		ArrayList<Salesman> arr_salesmen= new ArrayList(salesmen);
@@ -137,20 +180,26 @@ public class FileController {
 		}
 		return sale_info;
 	}
-
-	public static ArrayList<File> process_files(ArrayList<File> unprocessed_files) throws IOException{
+	
+	private static Map<String, Float> get_performance_by_salesman(Map<String, Float> salesmen_amount,
+			ArrayList<Salesman> salesmen) {
 		
-		for (File file : unprocessed_files){
-		   try{
-			   process_file(file);
-		   }catch (Exception e) {
-			   LogController.log_exception(e);
-		   }
-	    }
-		return null;
+		Map<String, Float> salesmen_performance = new HashMap<String, Float>();
+		
+		for (Map.Entry<String, Float> entry : salesmen_amount.entrySet()) {
+		    String salesman_name = entry.getKey();
+		    float amount = entry.getValue();
+		    float salary = get_salary_by_salesman_name(salesman_name, salesmen);
+		    System.out.println("salesman "+salesman_name); 
+		    System.out.println(salary);
+		    float performance = amount/salary;
+		    salesmen_performance.put(salesman_name, performance);
+		}
+		return salesmen_performance;
 	}
 	
-	public static void generate_output(ArrayList<Salesman> salesmen, ArrayList<Customer> customers, ArrayList<Sale> sales, String file_name) throws IOException{
+	
+	private static void generate_output(ArrayList<Salesman> salesmen, ArrayList<Customer> customers, ArrayList<Sale> sales, String file_name) throws IOException{
 		
 		
 		String customers_amount = String.valueOf(customers.size());
@@ -178,46 +227,6 @@ public class FileController {
 		
 	
 
-	}
-	
-	private static String get_worst_salesman(ArrayList<Sale> sales, ArrayList<Salesman> salesmen) {
-		// sum_sales/salary
-		Map<String, Float> salesmen_amount = get_salesmen_by_sales_amount(sales);
-		Map<String, Float> salesmen_performance = get_performance_by_salesman(salesmen_amount, salesmen);
-		
-		String worst_salesman_name = "";
-		float worst_salesman_performance = 1000000000;
-		
-		for (Map.Entry<String, Float> entry : salesmen_performance.entrySet()) {
-			
-			String actual_salesman_name = entry.getKey();
-			float actual_performance = entry.getValue();
-		    
-			if(actual_performance<worst_salesman_performance) {
-		    	worst_salesman_performance = actual_performance;
-		    	worst_salesman_name = actual_salesman_name;
-		    }
-			
-			System.out.println(entry.getKey() + "/" + entry.getValue());
-		}
-		return worst_salesman_name;
-	}
-	
-	private static Map<String, Float> get_performance_by_salesman(Map<String, Float> salesmen_amount,
-			ArrayList<Salesman> salesmen) {
-		
-		Map<String, Float> salesmen_performance = new HashMap<String, Float>();
-		
-		for (Map.Entry<String, Float> entry : salesmen_amount.entrySet()) {
-		    String salesman_name = entry.getKey();
-		    float amount = entry.getValue();
-		    float salary = get_salary_by_salesman_name(salesman_name, salesmen);
-		    System.out.println("salesman "+salesman_name); 
-		    System.out.println(salary);
-		    float performance = amount/salary;
-		    salesmen_performance.put(salesman_name, performance);
-		}
-		return salesmen_performance;
 	}
 	
 	private static float get_salary_by_salesman_name(String name, ArrayList<Salesman>salesmen) {
