29d6926fd5ac6ccf924239889ecf3cbc193469e5
mcbsf@cin.ufpe.br <mcbsf@cin.ufpe.br>
2020-10-17 22:35:15 -0300

    add log controller to manage errors

---
+++ src/controller/LogController.java
@@ -0,0 +1,16 @@
+package controller;
+
+public class LogController {
+	
+	public static boolean log_file_exception(Exception e) {
+		return true;
+	}
+	
+	public static boolean log_line_exception(Exception e) {
+		return true;
+	}
+	
+	public static boolean log_exception(Exception e) {
+		return true;
+	}
+}
